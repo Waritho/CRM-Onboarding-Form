33a78cc9300922ba3d968973e815b6c1348a22ff
Waritho <shubhagrawal070@gmail.com>
2026-03-02 20:18:52 +0530

    Add alembic configuration file and update script for FastAPI app startup

--- scrpit.sh
+++ scrpit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e  # Exit immediately if a command exits with non-zero status
+set -e
 
 echo "Running database migrations..."
 alembic -c alembic.ini upgrade head
 
 echo "Starting FastAPI app..."
-exec uvicorn main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port $PORT
