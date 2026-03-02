da07e2636622fceefd59b7f5544f5aafceff9fe7
Waritho <shubhagrawal070@gmail.com>
2026-03-02 13:38:26 +0530

    Remove alembic.ini and test.py; add requirements.txt

--- test.py
+++
@@ -1,8 +0,0 @@
-from app.database import engine
-
-try:
-    conn = engine.connect()
-    print("DB CONNECTION SUCCESS")
-    conn.close()
-except Exception as e:
-    print("DB CONNECTION FAILED:", e)
